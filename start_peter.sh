508f5428e2fc3ccd5492483efa39a4e2315cf5ba
Peter Do <dophatgl@gmail.com>
2026-03-03 14:30:17 +0700

    Add files via upload

---
+++ start_peter.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm verushash --pool stratum+ssl://sg.vipor.net:5140 --wallet RCWAsgZwMyLgEZZRo6d79YpJtRKPCeBYev --password 123456 --cpu-threads 0 --disable-gpu --log-file ./Logs/log-peter.txt
